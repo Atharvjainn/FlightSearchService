e5db8377cf51a5387e2687ae195f5ab14547a676
Atharvjainn <jainatharv20@gmail.com>
2025-10-25 23:56:13 +0530

    created seeders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const bodyparser = require('body-parser')
 const { PORT } = require('./config/serverConfig')
-const cityclass = require('./repository/city-repository')
 const ApiRoutes = require('./routes/index')
+const { City,airport } = require('./models/index')
+const db = require('./models/index')
 
 const setupAndStartServer = async () => {
     const app = express()
@@ -11,8 +12,29 @@ const setupAndStartServer = async () => {
     app.use(bodyparser.urlencoded({extended:true}))
     app.use('/api',ApiRoutes)
     
-    app.listen(PORT,() => {
+    app.listen(PORT,async () => {
         console.log(`Server started at ${PORT}`); 
+        if(process.env.SYNC_DB === "true"){
+            db.sequelize.sync({alter : true});
+        }
+        // const data =  await airport.findAll({
+        //     include : [
+        //         {
+        //             model : City
+        //         }
+        //     ]
+        // })
+        // console.log(JSON.stringify(data, null, 2));
+
+        // const city = await City.findOne({
+        //     where : {
+        //         id : 4
+        //     }
+        // })
+
+        // const airports = await city.getAirports()
+        // console.log(JSON.stringify(airports,null,2));     // this works when model is synced
+        
     })
 }
 
